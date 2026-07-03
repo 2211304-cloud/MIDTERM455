aa025dee2b50ae730b10961e998902466b8f4998
2211304-cloud <2211304@iub.edu.bd>
2026-07-04 01:38:14 +0600

    jss

---
+++ script.js
@@ -0,0 +1,18 @@
+function welcomeMessage(){
+
+alert("Welcome to Gadget Shop!");
+
+}
+
+
+function welcomeMessage(){
+
+alert("Welcome to Gadget Shop!");
+
+}
+
+function hideDetails(){
+
+document.getElementById("details").style.display="none";
+
+}
